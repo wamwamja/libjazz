46ab1d0d4f92bcc6299f64adc54415475822c9b1
jazz <jiazhen.luo@outlook.com>
2022-03-02 15:19:13 +0800

    [feature] First commit

---
+++ src/jazz/core/jazz_config.h
@@ -0,0 +1,15 @@
+//
+// Created by meitu on 2022/3/2.
+//
+
+#ifndef LIBJAZZ_JAZZ_CONFIG_H
+#define LIBJAZZ_JAZZ_CONFIG_H
+
+#include <cstring>
+#include <memory>
+
+namespace jazz{
+    using Real = double;
+}
+
+#endif //LIBJAZZ_JAZZ_CONFIG_H
